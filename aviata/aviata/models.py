0f8224c36cb5aec517a403a2375f5291599daa7a
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-18 00:57:05 +0600

    Flight model

--- aviata/aviata/models.py
+++ aviata/aviata/models.py
@@ -3,3 +3,12 @@ from django.db import models
 class Route(models.Model):
     from_code = models.CharField(max_length=10)
     to_code = models.CharField(max_length=10)
+
+class Flight(models.Model):
+    route = models.ForeignKey('Route', on_delete=models.CASCADE)
+    booking_token = models.TextField()
+    price = models.FloatField()
+    time = models.DateTimeField()
+    airline = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+    seats = models.IntegerField()
