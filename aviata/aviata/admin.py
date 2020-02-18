ecb72891ed45a64d56afafac36094fc29fa85482
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-18 15:06:46 +0600

    Added flights to admin panel

--- aviata/aviata/admin.py
+++ aviata/aviata/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from aviata.models import Route
+from aviata.models import Route, Flight
 
 class RouteAdmin(admin.ModelAdmin):
     pass
 
+class FlightAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Route, RouteAdmin)
+admin.site.register(Flight, FlightAdmin)
