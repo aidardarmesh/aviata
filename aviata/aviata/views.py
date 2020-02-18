6252345a41df8f06a3f357b04e26b9437d029599
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-18 17:57:18 +0600

    Flights retrieval

--- aviata/aviata/views.py
+++ aviata/aviata/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from aviata.models import Route, Flight
 from aviata.forms import RouteForm
+import datetime
 
 def index(request):
+    if request.method == 'POST':
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            date = form.cleaned_data['date']
+            till_date = date + datetime.timedelta(days=1)
+            route = Route.objects.get(pk=form.cleaned_data['route'])
+            flights = Flight.objects.filter(route=route, time__range=[date, till_date])
+
+            return render(request, 'flights.html', {'flights': flights})
+
     return render(request, 'index.html', {'form': RouteForm()})
 
