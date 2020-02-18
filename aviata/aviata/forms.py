adb648cdf6ef8f78be46a9cdd66ea8322edac670
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-18 16:23:17 +0600

    Rendered datepicker

--- aviata/aviata/forms.py
+++ aviata/aviata/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from aviata.models import Route
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class RouteForm(forms.Form):
     choices = [(route.pk, f"{route.from_code} -> {route.to_code}") for route in Route.objects.all()]
     route = forms.ChoiceField(choices=choices)
+    date = forms.DateField(widget=DateInput())
 
