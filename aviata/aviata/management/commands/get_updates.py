58436e2d49aedca50962fcebe5e03d3419886c3d
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-02-18 13:37:42 +0600

    Wrapped retrieval code into try/except block

--- aviata/aviata/management/commands/get_updates.py
+++ aviata/aviata/management/commands/get_updates.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from aviata.models import Route, Flight
 import datetime, requests
+import pytz
 
 class Command(BaseCommand):
     def valid_booking(self, token):
@@ -23,32 +24,42 @@ class Command(BaseCommand):
         routes = Route.objects.all()
         cur_date = datetime.datetime.today()
         dates = [cur_date + datetime.timedelta(days=x) for x in range(DAYS)]
+        local_tz = pytz.timezone("Asia/Almaty")
         
         for route in routes:
             for date in dates:
-                str_date = date.strftime("%d/%m/%Y")
-                print(route.from_code + " - " + route.to_code + " " + str_date)
-                
-                params = {
-                    'fly_from': route.from_code,
-                    'fly_to': route.to_code,
-                    'partner': 'picky',
-                    'date_from': str_date,
-                    'date_to': str_date,
-                }
-                data = requests.get(url=DATA_URL, params=params).json()
+                try:
+                    str_date = date.strftime("%d/%m/%Y")
+                    print(route.from_code + " - " + route.to_code + " " + str_date)
+                    
+                    params = {
+                        'fly_from': route.from_code,
+                        'fly_to': route.to_code,
+                        'partner': 'picky',
+                        'date_from': str_date,
+                        'date_to': str_date,
+                    }
+                    data = requests.get(url=DATA_URL, params=params).json()
 
-                for choice in data['data']:
-                    if self.valid_booking(choice['booking_token']) and choice['availability']:
-                        flight = Flight(
-                            route=route,
-                            booking_token=choice['booking_token'],
-                            price=choice['price'],
-                            time=choice['dTimeUTC'],
-                            airline=', '.join(choice['airlines']),
-                            duration=choice['fly_duration'],
-                            seats=choice['availability']
-                        )
-                        flight.save()
+                    for choice in data['data']:
+                        if self.valid_booking(choice['booking_token']) and choice['availability']['seats']:
+                            flight = Flight(
+                                route=route,
+                                booking_token=choice['booking_token'],
+                                price=choice['price'],
+                                time=local_tz.normalize(
+                                    datetime.datetime.utcfromtimestamp(choice['dTimeUTC']).astimezone(local_tz)
+                                ),
+                                airline=', '.join(choice['airlines']),
+                                duration=choice['fly_duration'],
+                                seats=choice['availability']['seats']
+                            )
+                            flight.save()
+                    except Exception as e:
+                        print(e)
+            
+                print('Finished these dates')
+            
+            print('Finished this route')
         
         print('Updating flights is finished.')
